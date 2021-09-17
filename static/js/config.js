7e2e0b2c704b48bbbb159b9827e5122488209e93
dianewitt <dianemarieak@gmail.com>
2021-09-17 09:41:28 -0500

    Create JavaScrip files

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+API_KEY = "pk.eyJ1IjoiZGlhbmVtYXJpZWFrIiwiYSI6ImNrazdrNnM0NTBlbjAyb250aGNucWlteXIifQ.TBqjjR0HvtnoXkFgh6xShw"
---
+++ static/js/weather.js
@@ -0,0 +1,18 @@
+const city = "chicago";
+
+// needs weatherAPI_KEY from config.js
+const weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI_KEY}&units=imperial`;
+
+// pulls in weather data then appends the information to a div using paragraph tags
+d3.json(weather_url).then((data) => {
+    var currentTemp = data.main.temp;
+    var feelsLike = data.main.feels_like;
+    var windSpeed = data.wind.speed;
+    var cloudCoverage = data.clouds.all;
+
+    // ties weather information to ID: #weather
+    d3.select("#weather").append("p").text(`Current Temperature: ${currentTemp}°F`);
+    d3.select("#weather").append("p").text(`Feels Like: ${feelsLike}°F`);
+    d3.select("#weather").append("p").text(`Wind Speed: ${windSpeed} MPH`);
+    d3.select("#weather").append("p").text(`Cloud Coverage: ${cloudCoverage}%`);
+})
